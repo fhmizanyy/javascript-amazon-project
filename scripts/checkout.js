249616575ee253a42825b9a5d6dc180daaccc273
fhmizany <fhmizany024@icloud.com>
2025-12-19 19:34:24 +0800

    Delete containerr

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
 
   cartSummaryHTML +=
 `        
-      <div class="cart-item-container">
+      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -105,8 +105,11 @@ document.querySelectorAll('.js-delete-link')
 .forEach((link) => {
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
-      removeFromCart(prooductId);
-      console.log(cart);
-
+      removeFromCart(productId);
+      
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      )
+      container.remove();
     });
 });
